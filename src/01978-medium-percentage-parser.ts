9b77f821ccde4df453b3e56bd958979f7b0a85dd
zhouzhenpan <zhouzhenpan@corp.netease.com>
2024-07-10 16:19:29 +0800

    feat: drop-char

---
+++ src/01978-medium-percentage-parser.ts
@@ -0,0 +1,45 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type Case0 = ['', '', '']
+type Case1 = ['+', '', '']
+type Case2 = ['+', '1', '']
+type Case3 = ['+', '100', '']
+type Case4 = ['+', '100', '%']
+type Case5 = ['', '100', '%']
+type Case6 = ['-', '100', '%']
+type Case7 = ['-', '100', '']
+type Case8 = ['-', '1', '']
+type Case9 = ['', '', '%']
+type Case10 = ['', '1', '']
+type Case11 = ['', '100', '']
+
+type cases = [
+  Expect<Equal<PercentageParser<''>, Case0>>,
+  Expect<Equal<PercentageParser<'+'>, Case1>>,
+  Expect<Equal<PercentageParser<'+1'>, Case2>>,
+  Expect<Equal<PercentageParser<'+100'>, Case3>>,
+  Expect<Equal<PercentageParser<'+100%'>, Case4>>,
+  Expect<Equal<PercentageParser<'100%'>, Case5>>,
+  Expect<Equal<PercentageParser<'-100%'>, Case6>>,
+  Expect<Equal<PercentageParser<'-100'>, Case7>>,
+  Expect<Equal<PercentageParser<'-1'>, Case8>>,
+  Expect<Equal<PercentageParser<'%'>, Case9>>,
+  Expect<Equal<PercentageParser<'1'>, Case10>>,
+  Expect<Equal<PercentageParser<'100'>, Case11>>
+]
+
+type CheckPrefix<T extends string> = T extends '+' | '-' ? T : ''
+
+type CheckSuffix<T extends string> = T extends `${infer P}%`
+  ? [P, '%']
+  : [T, '']
+
+// ============= Your Code Here =============
+// type PercentageParser<A extends string> = A extends `${CheckPrefix<
+//   infer L
+// >}${infer R}`
+//   ? [L, ...CheckSuffix<R>]
+//   : ['', ...CheckSuffix<A>]
+
+type A = PercentageParser<''>
