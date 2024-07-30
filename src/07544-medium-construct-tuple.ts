aa60c3d7acaf1c75603768b9494ce58140b8c07e
zhouzhenpan <zhouzhenpan@corp.netease.com>
2024-07-30 10:03:00 +0800

    feat: construct-tuple

---
+++ src/07544-medium-construct-tuple.ts
@@ -0,0 +1,18 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<ConstructTuple<0>, []>>,
+  Expect<Equal<ConstructTuple<2>, [unknown, unknown]>>,
+  Expect<Equal<ConstructTuple<999>['length'], 999>>,
+  // @ts-expect-error
+  Expect<Equal<ConstructTuple<1000>['length'], 1000>>
+]
+
+// ============= Your Code Here =============
+type ConstructTuple<
+  L extends number,
+  Result extends unknown[] = []
+> = Result['length'] extends L
+  ? Result
+  : ConstructTuple<L, [...Result, unknown]>
